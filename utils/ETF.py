efbc9dcb11761d1d332c8661e6caaadddb1e1d58
Manxi Sun <manxi.sun@tum.de>
2022-01-01 12:41:58 +0100

    add simple return

--- utils/ETF.py
+++ utils/ETF.py
@@ -1,9 +1,15 @@
-import networkx as nx
-from utils.PullData import PullData
-
 class ETF:
-    def __init__(self, ):
-        self.Graph = nx.DiGraph()
+    def __init__(self, data):
+        self.data = data
 
-    def Create_node(self, item):
-        F = 0
+    def simpleReturn(self, t, m):
+        r = self.data[1, t] / self.data[1, t - m] - 1
+        return r
+
+    def posinegaCal(self, tplusone):
+        rplusone = self.simpleReturn(tplusone, 1)
+        y = 0
+        if (rplusone >= 0):
+            y = 1
+        elif (rplusone < 0):
+            y = 0
