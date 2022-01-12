1752b3b63d6b0b70e48ec70e66ee8d9e0e11531d
Manxi Sun <manxi.sun@tum.de>
2022-01-12 15:47:54 +0100

    update

--- main.py
+++ main.py
@@ -7,10 +7,17 @@ if __name__ == '__main__':
     d = data['B']
     num = len(d)
     close = [item.value for item in d[1:num]]
-    print(close[0])
     yst = []
+    feature25 = []
     ETFdata = ETF(close)
-    for i in range(1,num-1):
+    for i in range(120,num-1):
         yst.append(ETFdata.posinegaCal(i))
+    for m in [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,40,60,80,100,120]:
+        tem = []
+        for i in range(120,num-1):
+            tem.append(ETFdata.simpleReturn(i,m))
+        feature25.append(tem)
+    print(feature25.size())
+
 
 
